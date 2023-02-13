19fa9db0f349cd3a961564306f60d3052dc75dd4
Przemyslaw Pietrasz <przemek681@gmail.com>
2023-02-13 21:24:53 +0100

    6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
 
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator(5,10);
+
+
+        int result1 = calculator.add();
+        int result2 = calculator.substract();
+
+        if (result1==calculator.add()){
+            System.out.println("OK!");
+        }
+        else {
+            System.out.println("Error");
+        }
+
+        if (result2==calculator.substract()){
+            System.out.println("OK!");
+        }
+        else {
+            System.out.println("Error");
+        }
+
+
+
     }
 }
