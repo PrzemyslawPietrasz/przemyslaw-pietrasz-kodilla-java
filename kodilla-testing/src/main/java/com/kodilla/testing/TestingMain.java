7848c52f0d66cc964805814f3c0fb7070c79ec3f
Przemyslaw Pietrasz <przemek681@gmail.com>
2023-02-14 09:03:06 +0100

    6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,20 +6,20 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator(5,10);
+        Calculator calculator = new Calculator();
 
 
-        int result1 = calculator.add();
-        int result2 = calculator.substract();
+        int result1 = calculator.add(5,10);
+        int result2 = calculator.substract(5,10);
 
-        if (result1==calculator.add()){
+        if (result1==calculator.add(5,10)){
             System.out.println("OK!");
         }
         else {
             System.out.println("Error");
         }
 
-        if (result2==calculator.substract()){
+        if (result2==calculator.substract(5,10)){
             System.out.println("OK!");
         }
         else {
