d1106282526f0f354ac31b477d625851b697c152
Przemyslaw Pietrasz <przemek681@gmail.com>
2023-03-02 22:06:57 +0100

    7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/StreamMain.java
@@ -1,56 +1,29 @@
 package com.kodilla.stream.beautifier;
 
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.reference.FunctionalCalculator;
+
+
+
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> mapOfForumUsers = forum.getForumUserList().stream()
+                .filter(forumUser -> forumUser.getSex()=='M')
+                .filter(forumUser->Period.between(forumUser.getBirthdayDate(), LocalDate.now()).getYears()>=20)
+                .filter(forumUser -> forumUser.getPostsQuantity()>=1)
+                .collect(Collectors.toMap(ForumUser::getId, ForumUser -> ForumUser));
 
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-
-        System.out.println("|Beautyfiler|");
-
-
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        String beautifulText = poemBeautifier.beautify("Text to beautify", (text -> text.toUpperCase()));
-        System.out.println(beautifulText);
-
-        String beautifulText2 = poemBeautifier.beautify("Text to beautify", (text -> "ABC" + text + "ABC"));
-        System.out.println(beautifulText2);
-
-        String beautifulText3 = poemBeautifier.beautify("Text to beautify", (text -> text.replace(" ", "_")));
-        System.out.println(beautifulText3);
-
-        String beautifulText4 = poemBeautifier.beautify("Text to beautify", (text -> text.length() + " letters ***" + text + "*** " + text.length() + " letters"));
-        System.out.println(beautifulText4);
-
-
-
-        String beautifulText5 = poemBeautifier.beautify("BIG LETTERS", (text -> text.toLowerCase()));
-        System.out.println(beautifulText5);
-
-
-        String beautifulText6 = poemBeautifier.beautify("BIG LETTERS ", (text -> text.repeat(text.length())));
-        System.out.println(beautifulText6);
-
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
-
+        mapOfForumUsers.entrySet().stream()
+                .map(entry -> entry.getKey() + ": "+ entry.getValue())
+                .forEach(System.out::println);
 
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -1,2 +1,24 @@
-package com.kodilla.stream.forumuser;public class Forum {
+package com.kodilla.stream.forumuser;
+
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Forum {
+    private final List<ForumUser> forumUserList = new ArrayList<>();
+
+    public Forum() {
+        forumUserList.add(new ForumUser(1, "Wojtek", 'M', LocalDate.of(1975, Month.DECEMBER, 9),30));
+        forumUserList.add(new ForumUser(2, "Ania", 'F', LocalDate.of(2000, Month.OCTOBER, 12),3));
+        forumUserList.add(new ForumUser(3, "Mateusz", 'M', LocalDate.of(1999, Month.DECEMBER, 10),4));
+        forumUserList.add(new ForumUser(4, "Jola", 'F', LocalDate.of(1987, Month.APRIL, 5),15));
+        forumUserList.add(new ForumUser(5, "Zygfryd", 'm', LocalDate.of(1974, Month.NOVEMBER, 17),2));
+
+    }
+
+    public List<ForumUser> getForumUserList() {
+        return new ArrayList<>(forumUserList);
+    }
 }
