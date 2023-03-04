07c49f5a5a836b9f7082072eba8668e35015efae
Przemyslaw Pietrasz <przemek681@gmail.com>
2023-03-04 19:30:20 +0100

    7.4

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -0,0 +1,53 @@
+package com.kodilla.stream.world;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class WorldTestSuite1 {
+
+    @Test
+    public void testGetPeopleQuantity(){
+        //Given
+        World world = new World();
+
+        Continent europa = new Continent("Europa");
+        Continent asia = new Continent("Asia");
+        Continent northAmerica = new Continent("NorthAmerica");
+
+        Country poland = new Country("Poland");
+        Country greece = new Country("Greece");
+        Country china = new Country("China");
+        Country japan = new Country("Japan");
+        Country uae = new Country("uae");
+        Country slovakia = new Country("Slovakia");
+        Country usa = new Country("USA");
+        Country canada = new Country("Canada");
+        Country russia = new Country("Russia");
+
+        world.addContinent(europa);
+        world.addContinent(asia);
+        world.addContinent(northAmerica);
+
+        europa.addCountry(poland);
+        europa.addCountry(greece);
+        europa.addCountry(slovakia);
+        europa.addCountry(russia);
+
+        asia.addCountry(japan);
+        asia.addCountry(uae);
+        asia.addCountry(china);
+        asia.addCountry(russia);
+
+        northAmerica.addCountry(usa);
+        northAmerica.addCountry(canada);
+
+        //When
+
+        BigDecimal totalPeople = world.getPeopleQuantity();
+        //Then
+        BigDecimal totalPeopleExpected = new BigDecimal("9000009");
+        Assert.assertEquals(totalPeopleExpected, totalPeople);
+    }
+}
