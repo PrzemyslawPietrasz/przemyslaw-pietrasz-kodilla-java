a9202f17626d581e016067eb9fd13c05796ed3c9
Przemyslaw Pietrasz <przemek681@gmail.com>
2023-03-04 20:16:29 +0100

    7.6

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,2 +1,17 @@
-package com.kodilla.stream.array;public class ArrayOperationsTestSuite {
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    public void testGetAverage(){
+        //Given
+        int numbers [] = {5,5,2,1,8,9};
+        double averageExpected = 5.0;
+        //When
+        double average = ArrayOperations.getAverage(numbers);
+        //Then
+        Assert.assertEquals(averageExpected, average, 0.0001);
+    }
 }
