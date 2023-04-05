1db9665b299c9fa51898b2ad1c597c96c07e4532
Przemyslaw Pietrasz <przemek681@gmail.com>
2023-04-05 19:52:50 +0200

    12.6

--- kodilla-jdbc/src/main/java/com/kodilla/kodillajdbc/com/kodilla/jdbc/DbManager.java
+++ kodilla-jdbc/src/main/java/com/kodilla/kodillajdbc/com/kodilla/jdbc/DbManager.java
@@ -14,13 +14,13 @@ public enum DbManager {
 
         DbManager() {                                              // [3]
             Properties connectionProps = new Properties();          // [4]
-            connectionProps.put("user", "kodilla_user");            // [5]
-            connectionProps.put("password", "kodilla_Pass123");     // [6]
+            connectionProps.put("user", "root");            // [5]
+            connectionProps.put("password", "Anubis1234");     // [6]
             try {
-                conn = DriverManager.getConnection(                  // [7]
+                conn = DriverManager.getConnection(
                         "jdbc:mysql://localhost:3306/kodilla_course" +
                                 "?serverTimezone=Europe/Warsaw" +
-                                "&useSSL=False",                                  // [10]
+                                "&useSSL=False&allowPublicKeyRetrieval=true",                                   // [10]
                         connectionProps);                                 // [11]
             } catch (SQLException e) {                              // [12]
                 throw new ExceptionInInitializerError(e);            // [13]
--- kodilla-jdbc/src/test/java/com/kodilla/kodillajdbc/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/kodillajdbc/jdbc/DbManagerTestSuite.java
@@ -2,9 +2,13 @@ package com.kodilla.kodillajdbc.jdbc;
 
 import com.kodilla.kodillajdbc.com.kodilla.jdbc.DbManager;
 import org.junit.jupiter.api.Test;
-import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class DbManagerTestSuite {
 
@@ -16,4 +20,68 @@ class DbManagerTestSuite {
         //Then
         assertNotNull(dbManager.getConnection());
     }
+
+
+    @Test
+    void testSelectUsers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        assertEquals(5, counter);
+    }
+        @Test
+        void testSelectUsersAndPosts() throws SQLException {
+        //GIVEN
+            DbManager dbManager = DbManager.getInstance();
+
+            //When
+
+
+
+            //THEN
+            String sqlQuery = """
+                                                    SELECT FIRSTNAME, LASTNAME
+                                                    FROM USERS
+                                                    WHERE ID IN (
+                                                        SELECT USER_ID
+                                                        FROM POSTS
+                                                        GROUP BY USER_ID
+                                                        HAVING COUNT(*) >= 2
+                                                    );""";
+            Statement statement = dbManager.getConnection().createStatement();
+            ResultSet rs = statement.executeQuery(sqlQuery);
+
+
+            //Then
+
+            int counter = 0;
+            while (rs.next()) {
+                System.out.println(
+                        rs.getString("FIRSTNAME") + ", " +
+                        rs.getString("LASTNAME"));
+                counter++;
+            }
+            rs.close();
+            statement.close();
+            assertEquals(2,counter);
+
+
+
+        }
+
 }
