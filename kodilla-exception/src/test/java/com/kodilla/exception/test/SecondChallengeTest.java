d39f3d5d0fb88c14ec36e928bc35fb887721ada7
PrzemyslawPietrasz <Przemek681@gmail.com>
2023-06-18 23:13:41 +0200

    23.3

--- kodilla-exception/src/test/java/com/kodilla/exception/test/SecondChallengeTest.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/SecondChallengeTest.java
@@ -1,4 +1,20 @@
+package com.kodilla.exception.test;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class SecondChallengeTest {
-  
-}
+
+public class SecondChallengeTest {
+
+    @Test
+    void probablyIWillThrowException(){
+        //Given
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        //When & Then
+        assertAll(
+                ()-> assertThrows(Exception.class, ()-> secondChallenge.probablyIWillThrowException(2,1.5)),
+                ()-> assertThrows(Exception.class, ()-> secondChallenge.probablyIWillThrowException(1,1.5)),
+                ()-> assertDoesNotThrow(()-> secondChallenge.probablyIWillThrowException(1,1))
+        );
+    }}
